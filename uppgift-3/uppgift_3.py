5e29b31f1ecddb2c4bb60b7b062eaea64acaf0a9
anmol-amo <i_pearl55@yahoo.com>
2025-01-07 11:40:40 +0100

    update

--- uppgift-3/uppgift_3.py
+++ uppgift-3/uppgift_3.py
@@ -1,8 +1,16 @@
 # Uppgift 3
 # Hitta det största talet i en lista
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def max_in_list(numbers):
+
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    
+    return max(numbers)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+print(max_in_list(numbers)) 
+
+
