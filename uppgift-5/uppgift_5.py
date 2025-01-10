a740c785f8b04d97ebcf75fe03b8e54e35639b57
anmol-amo <i_pearl55@yahoo.com>
2025-01-10 10:55:21 +0100

    uppgift

--- uppgift-5/uppgift_5.py
+++ uppgift-5/uppgift_5.py
@@ -1,8 +1,23 @@
 # Uppgift 5
 # Skapa en funktion filter_odd(numbers) som returnerar en lista med alla jämna tal från den givna listan.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def filter_odd(numbers):
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    
+
+
+
+num_list = [1, 2, 3, 4]
+runs = 0
+odds = []
+
+for n in num_list:
+    if n % 2 == 0:
+
+        odds.append(n)
+        runs += 1
+        if runs == 5:
+            break
+print(odds)
