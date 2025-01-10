857eb4928d3910546afa31754aca0a16ff12946d
anmol-amo <i_pearl55@yahoo.com>
2025-01-10 20:25:56 +0100

    uppgift

--- uppgift-6/uppgift_6.py
+++ uppgift-6/uppgift_6.py
@@ -1,8 +1,23 @@
 # Uppgift 6
 # Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def multiplication_table(n, limit):
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+ 
+    if limit <= 0:
+        return "Limit måste vara ett positivt heltal"
+    
+    # Skapa multiplikationstabellen
+    table = []
+    for i in range(1, limit + 1):
+        table.append(n * i)
+    
+    return table
+
+n = 5
+limit = 10
+print(multiplication_table(n, limit)) 
+print(multiplication_table(2, 3))
+print(multiplication_table(3, 5))
