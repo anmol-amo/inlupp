a3069309416d55ef38ab8dac171331f21d1b52ff
anmol-amo <i_pearl55@yahoo.com>
2025-01-13 09:25:46 +0100

    uppgift

--- uppgift-8/uppgift_8.py
+++ uppgift-8/uppgift_8.py
@@ -1,8 +1,30 @@
 # Uppgift 8
 # Skapa en funktion count_letters(string) som returnerar en dictionary med varje bokstav som nyckel och antalet förekomster som värde.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
+def count_letters(string):
+
+    letter_count = {}
+
+    for char in string:
+
+        if char.isalpha():  
+
+            char = char.lower()  
+
+            if char in letter_count:
+
+                letter_count[char] += 1
+
+            else:
+
+                letter_count[char] = 1
+
+    return letter_count
+ 
+ 
+print(count_letters("hello"))
+
+print(count_letters(""))
+
+print(count_letters("aaa"))
+ 
